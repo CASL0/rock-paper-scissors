430e900f84963aa17764c7f8e107e08a6c2abdaa
CASL0 <gin.0046.822@gmail.com>
2024-08-11 16:45:21 +0900

    feat: 各要素を配置

--- app/src/main/java/io/github/casl0/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/io/github/casl0/rockpaperscissors/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextButton
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.casl0.rockpaperscissors.ui.common.Nameplates
+import io.github.casl0.rockpaperscissors.ui.common.RpsButtons
 import io.github.casl0.rockpaperscissors.ui.theme.RockPaperScissorsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             RockPaperScissorsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Nameplates(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp)
+                        )
+                        Text(
+                            text = ":",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            fontSize = 80.sp
+                        )
+                        RpsButtons(modifier = Modifier.fillMaxWidth())
+                        TextButton(
+                            onClick = { /* TODO: implement */ },
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.primary)
+                        ) {
+                            Text(
+                                text = stringResource(R.string.find),
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RockPaperScissorsTheme {
-        Greeting("Android")
     }
 }
