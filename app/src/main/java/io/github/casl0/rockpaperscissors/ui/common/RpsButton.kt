fe6635b1a41a2b231c1adbef567feb66dbddd79c
CASL0 <gin.0046.822@gmail.com>
2024-08-11 16:11:10 +0900

    feat: グーチョキパーのボタン

--- app/src/main/java/io/github/casl0/rockpaperscissors/ui/common/RpsButton.kt
+++ app/src/main/java/io/github/casl0/rockpaperscissors/ui/common/RpsButton.kt
@@ -2,7 +2,12 @@ package io.github.casl0.rockpaperscissors.ui.common
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +15,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.casl0.rockpaperscissors.R
 
 @Composable
-internal fun RpsButton(
+private fun RpsButton(
     @DrawableRes drawableResource: Int,
     name: String,
     onClick: () -> Unit,
@@ -23,7 +31,7 @@ internal fun RpsButton(
 ) {
     IconButton(
         onClick = onClick,
-        modifier = modifier
+        modifier = modifier.background(MaterialTheme.colorScheme.tertiary)
     ) {
         Column(
             modifier = Modifier.wrapContentHeight(),
@@ -31,11 +39,13 @@ internal fun RpsButton(
         ) {
             Image(
                 painter = painterResource(drawableResource),
-                contentDescription = name
+                contentDescription = name,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onTertiary)
             )
             Text(
                 text = name,
-                style = MaterialTheme.typography.labelSmall
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onTertiary
             )
         }
     }
@@ -46,5 +56,41 @@ internal fun RpsButton(
 private fun RpsButtonPreview() {
     MaterialTheme {
         RpsButton(R.drawable.rock, "ROCK", {})
+    }
+}
+
+@Composable
+internal fun RpsButtons(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        RpsButton(
+            drawableResource = R.drawable.rock,
+            name = stringResource(R.string.rock),
+            onClick = { /* TODO: implement */ },
+            modifier = Modifier.width(80.dp)
+        )
+        RpsButton(
+            drawableResource = R.drawable.paper,
+            name = stringResource(R.string.paper),
+            onClick = { /* TODO: implement */ },
+            modifier = Modifier.width(80.dp)
+
+        )
+        RpsButton(
+            drawableResource = R.drawable.scissors,
+            name = stringResource(R.string.scissors),
+            onClick = { /* TODO: implement */ },
+            modifier = Modifier.width(80.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RpsButtonsPreview() {
+    MaterialTheme {
+        RpsButtons(modifier = Modifier.fillMaxWidth())
     }
 }
